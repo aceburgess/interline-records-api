245a5043461d5b434949131555e5f3041aa312ad
Ace Burgess <burgess.ace@gmail.com>
2017-03-11 14:15:15 -0500

    Add makemigrations to startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 python /usr/src/app/manage.py collectstatic --noinput
+python /usr/src/app/manage.py makemigrations
 python /usr/src/app/manage.py migrate
 /usr/local/bin/gunicorn interlineapi.wsgi:application -w 2 -b :8000
